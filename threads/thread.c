ad17f4e8c5f2939691119bef3376fe8c377c269b
Won-n-only <siliver12@gmail.com>
2024-04-29 12:01:59 +0900

    Fix: thread_yield 함수 버그 수정(#4)
    Bug: list_insert_ordered 인자 수정
    Co-authored-by: 최준성 <147041315+yumXD@users.noreply.github.com>
    Co-authored-by: makeit-jy <makeit_jy@naver.com>

--- threads/thread.c
+++ threads/thread.c
@@ -336,7 +336,7 @@ void thread_yield(void) {
 
     old_level = intr_disable();
     if (curr != idle_thread)
-        list_insert_ordered(&ready_list, &curr->priority, cmp_priority, NULL);
+        list_insert_ordered(&ready_list, &curr->elem, cmp_priority, NULL);
     do_schedule(THREAD_READY);
     intr_set_level(old_level);
 }
