a6f88420ee58e9db6818397dd216aade657f6860
Won-n-only <siliver12@gmail.com>
2024-04-29 21:58:30 +0900

    Feat: cond_signal() 함수 수정(#6)
    condition variable의 waiters list를 우선순위로 재정렬
    
    Co-authored-by: 최준성 <147041315+yumXD@users.noreply.github.com>
    Co-authored-by: makeit-jy <makeit_jy@naver.com>

--- threads/synch.c
+++ threads/synch.c
@@ -295,8 +295,10 @@ void cond_signal(struct condition *cond, struct lock *lock UNUSED) {
     ASSERT(!intr_context());
     ASSERT(lock_held_by_current_thread(lock));
 
-    if (!list_empty(&cond->waiters))
+    if (!list_empty(&cond->waiters)) {
+        list_sort(&cond->waiters, cmp_sem_priority, NULL);
         sema_up(&list_entry(list_pop_front(&cond->waiters), struct semaphore_elem, elem)->semaphore);
+    }
 }
 
 /* Wakes up all threads, if any, waiting on COND (protected by
