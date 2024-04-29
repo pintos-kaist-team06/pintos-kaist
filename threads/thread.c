040ae8902124d7ea03057bd57841c6d6af2a6428
Won-n-only <siliver12@gmail.com>
2024-04-29 10:50:50 +0900

    Feat: block된 스레드를 ready 상태로 전환 시 우선순위로 ready_list에 삽입하기(#4)
    Co-authored-by: 최준성 <147041315+yumXD@users.noreply.github.com>
    Co-authored-by: makeit-jy <makeit_jy@naver.com>

--- threads/thread.c
+++ threads/thread.c
@@ -270,7 +270,7 @@ void thread_unblock(struct thread *t) {
 
     old_level = intr_disable();
     ASSERT(t->status == THREAD_BLOCKED);
-    list_push_back(&ready_list, &t->elem);
+    list_insert_ordered(&ready_list, &t->elem, cmp_priority, NULL);
     t->status = THREAD_READY;
     intr_set_level(old_level);
 }
