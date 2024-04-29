7f2bdde9bcb81bf24b207cf1741cec889b37f8ee
Won-n-only <siliver12@gmail.com>
2024-04-29 10:56:40 +0900

    Feat: 우선순위에 따라 CPU 양보하도록 수정하기(#4)
    Co-authored-by: 최준성 <147041315+yumXD@users.noreply.github.com>
    Co-authored-by: makeit-jy <makeit_jy@naver.com>

--- threads/thread.c
+++ threads/thread.c
@@ -335,7 +335,7 @@ void thread_yield(void) {
 
     old_level = intr_disable();
     if (curr != idle_thread)
-        list_push_back(&ready_list, &curr->elem);
+        list_insert_ordered(&ready_list, &curr->priority, cmp_priority, NULL);
     do_schedule(THREAD_READY);
     intr_set_level(old_level);
 }
