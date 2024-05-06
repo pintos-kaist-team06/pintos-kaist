17581595202068595f36f51c4efb63c894ad1d38
Won-n-only <siliver12@gmail.com>
2024-05-06 17:25:52 +0900

    Feat: 기본 시스템콜 4개 구현 (#14)
    SYS_HALT, SYS_EXIT, SYS_CREATE, SYS_REMOVE 구현 완료
    
    Co-authored-by: 최준성 <147041315+yumXD@users.noreply.github.com>
    Co-authored-by: makeit-jy <makeit_jy@naver.com>

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -13,6 +13,9 @@
 void syscall_entry(void);
 void syscall_handler(struct intr_frame *);
 void check_address(void *uaddr);
+void halt(void);
+bool create(const char *file, unsigned initial_size);
+bool remove(const char *file);
 
 /* System call.
  *
@@ -39,9 +42,61 @@ void syscall_init(void) {
 
 /* The main system call interface */
 void syscall_handler(struct intr_frame *f UNUSED) {
-    // TODO: Your implementation goes here.
-    printf("system call!\n");
-    thread_exit();
+    check_address(f->R.rax);  // 임시
+    uint64_t syscall_n = f->R.rax;
+
+    switch (syscall_n) {
+        case SYS_HALT:
+            halt();
+            break;
+
+        case SYS_EXIT:
+            exit(f->R.rdi);
+            break;
+
+        case SYS_FORK:
+            break;
+
+        case SYS_EXEC:
+            break;
+
+        case SYS_WAIT:
+            break;
+
+        case SYS_CREATE:
+            f->R.rax = create(f->R.rdi, f->R.rsi);
+            break;
+
+        case SYS_REMOVE:
+            f->R.rax = remove(f->R.rdi);
+            break;
+
+        case SYS_OPEN:
+            break;
+
+        case SYS_FILESIZE:
+            break;
+
+        case SYS_READ:
+            break;
+
+        case SYS_WRITE:
+            printf(f->R.rdi);
+            break;
+
+        case SYS_SEEK:
+            break;
+
+        case SYS_TELL:
+            break;
+
+        case SYS_CLOSE:
+            break;
+
+        default:
+            exit(-1);
+            break;
+    }
 }
 
 void check_address(void *uaddr) {
@@ -54,4 +109,21 @@ void check_address(void *uaddr) {
 void exit(int status) {
     printf("%s: exit(%d)\n", thread_name(), status);
     thread_exit();
+}
+
+void halt(void) {
+    power_off();
+}
+
+bool create(const char *file, unsigned initial_size) {
+    check_address(file);
+    bool is_success = filesys_create(file, initial_size);
+
+    return is_success;
+}
+
+bool remove(const char *file) {
+    check_address(file);
+    bool is_success = filesys_remove(file);
+    return is_success;
 }
